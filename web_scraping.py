b374145264ba9e29c1473c6fe450cff932c87dcb
Kevin Benhamou <kevbenhamou@gmail.com>
2022-12-08 16:03:14 +0200

    milestone 3

--- web_scraping.py
+++ web_scraping.py
@@ -2,6 +2,9 @@ import requests
 import re
 from bs4 import BeautifulSoup
 from imdb_db import update_global_table
+import json
+import ast
+import logging
 
 
 def check_item(movie_or_tv, year, genre, movie_or_tv_filter, genre_filter, year_beg_filter, year_end_filter):
@@ -9,8 +12,6 @@ def check_item(movie_or_tv, year, genre, movie_or_tv_filter, genre_filter, year_
     if movie_or_tv != movie_or_tv_filter:
         return False
     if genre_filter != 'All' and genre_filter not in genre:
-        #print(genre)
-        #print(genre_filter.split(','))
         return False
     if year < year_beg_filter or year > year_end_filter:
         return False
@@ -45,15 +46,75 @@ def get_cast(soup):
     return list(set(production_team)), list(set(actors))
 
 
+def get_movie_data(movie, args, headers, api_key):
+    """This function allows to retrieve the data from the web"""
+    title = movie.find(class_="lister-item-header").a.string
+    movie_url = movie.find(class_="lister-item-header").a['href']
+    omdb_id = movie_url.split('/')[2]
+    movie_id = int(''.join(c for c in movie_url if c.isdigit()))
+    genre = movie.find(class_="genre").string.strip().split(',')
+    year = movie.find(class_='text-muted').string[1:-1]
+    release_year = int(re.findall('[0-9]{4}', year)[0])
+    if movie.strong:
+        rating = float(movie.strong.string)
+    else:
+        rating = None
+    resume = movie.find_all('p')[1].string
+    if resume:
+        resume = resume.strip()
+
+    movie_page = requests.get('https://www.imdb.com' + movie_url, headers=headers)
+
+    if movie_page.status_code != 200:
+        logging.error("URL doesn't exist ! Failed")
+
+    movie_soup = BeautifulSoup(movie_page.content, "html.parser")
+
+    if movie_soup.find(role="presentation", class_="ipc-inline-list__item", string="TV Series"):
+        movie_or_tv = "TV"
+    else:
+        movie_or_tv = "Movie"
+
+    if check_item(movie_or_tv, release_year, genre, args.movie_or_tv, args.genre, args.year_beg, args.year_end):
+        prod, actors = get_cast(movie_soup)
+        rotten_rating, meta_rating = get_ratings(omdb_id, api_key)
+
+        return movie_id, title, movie_or_tv, release_year, genre, rating, resume, prod, actors, rotten_rating, meta_rating
+    else:
+        return None
+
+
+def get_ratings(movie_id, api_key):
+    omdb_url = 'http://www.omdbapi.com/?apikey=' + api_key + '&i=' + movie_id
+    page = requests.get(omdb_url)
+    if page.status_code != 200:
+        logging.error("URL doesn't exist ! Failed")
+    movie_json = page.json()
+    if movie_json['Ratings'] != []:
+        rotten_rating = movie_json['Ratings'][1]['Value']
+        meta_rating = movie_json['Ratings'][2]['Value']
+        rotten_rating = float(rotten_rating.split('%')[0])/10
+        meta_rating = float(meta_rating.split('/')[0])/10
+    else:
+        rotten_rating = None
+        meta_rating = None
+
+    return rotten_rating, meta_rating
+
+
 def data_retrieving(args, my_url):
     """This function gets an URL from one of the categories of IMDB's website and prints each movie/tv show with
     relevant datas. """
+    with open('conf_IMDB.json') as json_config:
+        config = json.load(json_config)
+        headers = ast.literal_eval(config['HEADERS'])
+        api_key = config['API_KEY']
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/102.0.0.0 Safari/537.36'}
     page = requests.get(my_url, headers=headers)
-    assert page.status_code == 200
+
+    if page.status_code != 200:
+        logging.error("URL doesn't exist ! Failed")
+
     while page:
         soup = BeautifulSoup(page.content, "html.parser")
         next_url = 'https://www.imdb.com' + soup.find(class_="desc").find_all('a')[-1]['href']
@@ -61,39 +122,10 @@ def data_retrieving(args, my_url):
 
         movie = movies_list.find(class_="lister-item")
         while movie:
-            title = movie.find(class_="lister-item-header").a.string
-            movie_url = movie.find(class_="lister-item-header").a['href']
-            movie_id = int(''.join(c for c in movie_url if c.isdigit()))
-            genre = movie.find(class_="genre").string.strip().split(',')
-            year = movie.find(class_='text-muted').string[1:-1]
-            release_year = int(re.findall('[0-9]{4}', year)[0])
-            if movie.strong:
-                rating = float(movie.strong.string)
-            else:
-                rating = "NULL"
-            resume = movie.find_all('p')[1].string
-            if resume:
-                resume = resume.strip()
-
-            movie_page = requests.get('https://www.imdb.com' + movie_url, headers=headers)
-            assert movie_page.status_code == 200
-            movie_soup = BeautifulSoup(movie_page.content, "html.parser")
-
-            if movie_soup.find(role="presentation", class_="ipc-inline-list__item", string="TV Series"):
-                movie_or_tv = "TV"
-            else:
-                movie_or_tv = "Movie"
-
-            if check_item(movie_or_tv, release_year, genre, args.movie_or_tv, args.genre, args.year_beg, args.year_end):
-                prod, actors = get_cast(movie_soup)
-                #print(title)
-                #print(movie_id)
-                #print(prod)
-                #print(actors)
-                #print("\n")
-                update_global_table(movie_id, title, movie_or_tv, release_year, genre, rating, resume, prod, actors)
-
-
+            data = get_movie_data(movie, args, headers, api_key)
+            if data:
+                update_global_table(*data)
+                logging.info(f'Retrieving movie :{data[1]}')
             movie = movie.next_sibling.next_sibling
 
         page = requests.get(next_url, headers=headers)
