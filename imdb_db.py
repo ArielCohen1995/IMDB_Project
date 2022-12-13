eb2d1d9bfc6e3ca7a203695db5c9535b75c65397
Ariel Cohen <116627393+ArielCohen1995@users.noreply.github.com>
2022-12-13 15:21:27 +0200

    Update imdb_db.py
    Milestone4

--- imdb_db.py
+++ imdb_db.py
@@ -7,7 +7,7 @@ def check_DB_exist():
         config = json.load(json_config)
         connection = pymysql.connect(user=config['USER'], password=config['PASSWORD'], host=config['HOST'])
         cursor = connection.cursor()
-        cursor.execute("SHOW DATABASES LIKE 'IMDb_DB';")
+        cursor.execute("SHOW DATABASES LIKE 'ariel_kevin';")
 
     if not cursor.fetchall():
         return False
@@ -23,13 +23,13 @@ def init_DB():
         cursor = connection.cursor()
 
     try:
-        #cursor.execute("DROP DATABASE IMDb_DB")
+        #cursor.execute("DROP DATABASE ariel_kevin")
         #connection.commit()
 
-        cursor.execute("CREATE DATABASE IMDb_DB")
+        cursor.execute("CREATE DATABASE ariel_kevin")
         connection.commit()
 
-        cursor.execute("CREATE TABLE IMDb_DB.global ("
+        cursor.execute("CREATE TABLE ariel_kevin.global ("
                        "id INT PRIMARY KEY,"
                        "title VARCHAR(150),"
                        "type_input VARCHAR(150),"
@@ -39,33 +39,33 @@ def init_DB():
                        "rating_Metacritic REAL)")
         connection.commit()
 
-        cursor.execute("CREATE TABLE IMDb_DB.genres ("
+        cursor.execute("CREATE TABLE ariel_kevin.genres ("
                        "movie_id INT,"
                        "genre VARCHAR(150))")
         connection.commit()
 
-        cursor.execute("CREATE TABLE IMDb_DB.synopsis ("
+        cursor.execute("CREATE TABLE ariel_kevin.synopsis ("
                        "movie_id INT PRIMARY KEY,"
                        "resume TEXT)")
         connection.commit()
 
-        cursor.execute("CREATE TABLE IMDb_DB.actors ("
+        cursor.execute("CREATE TABLE ariel_kevin.actors ("
                        "id INT ,"
                        "movie_id INT)")
         connection.commit()
 
-        cursor.execute("CREATE TABLE IMDb_DB.actors_details ("
+        cursor.execute("CREATE TABLE ariel_kevin.actors_details ("
                        "id INT PRIMARY KEY,"
                        "name VARCHAR(150))")
         connection.commit()
 
-        cursor.execute("CREATE TABLE IMDb_DB.producers ("
+        cursor.execute("CREATE TABLE ariel_kevin.producers ("
                        "movie_id INT,"
                        "id INT,"
                        "work VARCHAR(150))")
         connection.commit()
 
-        cursor.execute("CREATE TABLE IMDb_DB.producers_details ("
+        cursor.execute("CREATE TABLE ariel_kevin.producers_details ("
                        "id INT PRIMARY KEY,"
                        "name VARCHAR(150))")
         connection.commit()
@@ -82,36 +82,36 @@ def update_global_table(movie_id, title, movie_or_tv, release_year, genre, ratin
         cursor = connection.cursor()
 
     try:
-        cursor.execute("INSERT INTO IMDb_DB.global (id, title, type_input, year_released, rating_IMDB, rating_Rot_To, rating_Metacritic) "
+        cursor.execute("INSERT INTO ariel_kevin.global (id, title, type_input, year_released, rating_IMDB, rating_Rot_To, rating_Metacritic) "
                        "VALUES (%s, %s, %s, %s, %s, %s, %s);", (movie_id, title, movie_or_tv, release_year, rating_IMDB, rating_Rot_To, rating_Metacritic))
         connection.commit()
 
         for g in genre:
-            cursor.execute("INSERT INTO IMDb_DB.genres (movie_id, genre) "
+            cursor.execute("INSERT INTO ariel_kevin.genres (movie_id, genre) "
                            "VALUES (%s, %s);", (movie_id, g))
             connection.commit()
 
-        cursor.execute("INSERT INTO IMDb_DB.synopsis (movie_id, resume) "
+        cursor.execute("INSERT INTO ariel_kevin.synopsis (movie_id, resume) "
                        "VALUES (%s, %s);", (movie_id, resume))
         connection.commit()
 
         for actor in actors:
-            cursor.execute("INSERT INTO IMDb_DB.actors (id, movie_id) "
+            cursor.execute("INSERT INTO ariel_kevin.actors (id, movie_id) "
                            "VALUES (%s, %s);", (actor[1], movie_id))
 
-            cursor.execute("INSERT INTO IMDb_DB.actors_details (id, name) "
+            cursor.execute("INSERT INTO ariel_kevin.actors_details (id, name) "
                            "VALUES (%s, %s);", (actor[1], actor[0]))
 
         for staff in prod:
-            cursor.execute("INSERT INTO IMDb_DB.producers (movie_id, id, work) "
+            cursor.execute("INSERT INTO ariel_kevin.producers (movie_id, id, work) "
                            "VALUES (%s, %s, %s);", (movie_id, staff[1], staff[2]))
 
-            cursor.execute("INSERT INTO IMDb_DB.producers_details (id, name) "
+            cursor.execute("INSERT INTO ariel_kevin.producers_details (id, name) "
                            "VALUES (%s, %s);", (staff[1], staff[0]))
 
     except pymysql.err.IntegrityError:
         cursor.execute(
-            "UPDATE IMDb_DB.global "
+            "UPDATE ariel_kevin.global "
             "SET "
             "title = %s, "
             "type_input = %s, "
@@ -124,14 +124,14 @@ def update_global_table(movie_id, title, movie_or_tv, release_year, genre, ratin
 
         for g in genre:
             cursor.execute(
-                "UPDATE IMDb_DB.genres "
+                "UPDATE ariel_kevin.genres "
                 "SET "
                 "genre = %s "
                 "WHERE movie_id = %s", (g, movie_id))
             connection.commit()
 
         cursor.execute(
-            "UPDATE IMDb_DB.synopsis "
+            "UPDATE ariel_kevin.synopsis "
             "SET "
             "resume = %s "
             "WHERE movie_id = %s", (resume, movie_id))
@@ -139,14 +139,14 @@ def update_global_table(movie_id, title, movie_or_tv, release_year, genre, ratin
 
         for actor in actors:
             cursor.execute(
-                "UPDATE IMDb_DB.actors "
+                "UPDATE ariel_kevin.actors "
                 "SET "
                 "id = %s "
                 "WHERE movie_id = %s", (actor[1], movie_id))
             connection.commit()
 
             cursor.execute(
-                "UPDATE IMDb_DB.actors_details "
+                "UPDATE ariel_kevin.actors_details "
                 "SET "
                 "name = %s "
                 "WHERE id = %s", (actor[0], actor[1]))
@@ -154,7 +154,7 @@ def update_global_table(movie_id, title, movie_or_tv, release_year, genre, ratin
 
         for staff in prod:
             cursor.execute(
-                "UPDATE IMDb_DB.producers "
+                "UPDATE ariel_kevin.producers "
                 "SET "
                 "id = %s, "
                 "work = %s "
@@ -162,7 +162,7 @@ def update_global_table(movie_id, title, movie_or_tv, release_year, genre, ratin
             connection.commit()
 
             cursor.execute(
-                "UPDATE IMDb_DB.producers_details "
+                "UPDATE ariel_kevin.producers_details "
                 "SET "
                 "name = %s "
                 "WHERE id = %s", (staff[0], staff[1]))
